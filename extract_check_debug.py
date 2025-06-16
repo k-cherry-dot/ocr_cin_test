d79d54e617eb834fc0223a24c636c8def0a05eaf
haru-13-hi <kenza.rhardisse@gmail.com>
2025-06-16 11:39:17 +0200

    multiple_names

--- extract_check_debug.py
+++ extract_check_debug.py
@@ -5,7 +5,7 @@ import pandas as pd
 from datetime import datetime
 from difflib import SequenceMatcher
 
-#debugged for multiple given names and surnames
+#now debugged for multiple given names and surnames
 
 
 pytesseract.pytesseract.tesseract_cmd = r"C:\Program Files\Tesseract-OCR\tesseract.exe"
